e5473965a5633bd1ec50646038baa1d75610dfc4
HerrIves <spechtator@gmail.com>
2023-02-03 00:18:48 +0100

    Add ShoppingCardTest

---
+++ taxes/src/test/java/shoppingcard/ShoppingCardTest.java
@@ -0,0 +1,14 @@
+package shoppingcard;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ShoppingCardTest {
+
+    @Test
+    public void isBaskedExist(){
+        ShoppingCard card = new ShoppingCard();
+
+        Assert.assertTrue(card.getBascked() != null);
+    }
+}
