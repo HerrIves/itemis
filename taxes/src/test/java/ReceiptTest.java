3b9dc5a7ed5ef81e37f8712fb257e047d1e40544
HerrIves <spechtator@gmail.com>
2023-02-02 20:40:06 +0100

    add tests for cd & bottle

--- taxes/src/test/java/ReceiptTest.java
+++ taxes/src/test/java/ReceiptTest.java
@@ -1,4 +1,6 @@
 import items.Book;
+import items.CD;
+import items.ImportedBottle;
 import items.Item;
 import org.junit.Assert;
 import org.junit.Test;
@@ -6,12 +8,30 @@ import org.junit.Test;
 public class ReceiptTest {
 
     @Test
-    public void receiptOutShouldBeEqualToInput(){
-        Item item = new Book();
-        item.setPrice(12.49);
-        item.setIsExempt(true);
-//        item.setIsImport(false);
+    public void taxesForBookTest(){
+        Book book = new Book();
+        book.setPrice(12.49);
+        book.setIsExempt(true);
 
-        Assert.assertEquals(12.49, item.getPrice()+Receipt.taxesOutForItem(item), 0.001);
+        Assert.assertEquals(12.49, book.getPrice()+Receipt.taxesOutForItem(book), 0.001);
+    }
+
+    @Test
+    public void taxesForCDTest(){
+        CD cd = new CD();
+        cd.setPrice(14.99);
+//        item.setIsExempt(true);
+
+        Assert.assertEquals(16.49, cd.getPrice()+Receipt.taxesOutForItem(cd), 0.001);
+    }
+
+    @Test
+    public void taxesForImportedBottleTest(){
+        ImportedBottle bottle = new ImportedBottle();
+        bottle.setPrice(47.50);
+        bottle.setIsExempt(true);
+        bottle.setIsImport(true);
+
+        Assert.assertEquals(54.65, bottle.getPrice()+Receipt.taxesOutForItem(bottle), 0.001);
     }
 }
