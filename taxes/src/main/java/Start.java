42b10e942f1849cc4f4864d9da8875ac817b40fd
HerrIves <spechtator@gmail.com>
2023-02-07 22:38:41 +0100

    Rework Start.class

--- taxes/src/main/java/Start.java
+++ taxes/src/main/java/Start.java
@@ -1,17 +1,10 @@
-import items.Book;
-import items.CD;
-import items.ChocolateBar;
-import shoppingcard.ShoppingCard;
-import shoppingreceipt.Receipt;
+import input.Input;
+
+import java.util.List;
 
 public class Start {
     public static void main(String[] args) {
-        ShoppingCard card = new ShoppingCard();
-        card.addItem(new Book("am westen nichts neues", 12.49));
-        card.addItem(new CD("Sting", 14.99));
-        card.addItem(new ChocolateBar("Snickers", 0.85));
-
-        Receipt receipt = new Receipt(card);
-        receipt.output();
+        List lines = Input.readLines(SystemInOut.getInput);
+                Input.proceedEach.getOutput(SystemInOut.getOut);
     }
 }
