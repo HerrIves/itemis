dfe0e08d73e980f7b4945b779e62c9f293962c0e
HerrIves <spechtator@gmail.com>
2023-02-13 06:00:01 +0100

    Update api-key

--- taxes/src/main/java/de/herrives/config/ChadGPTConfig.java
+++ taxes/src/main/java/de/herrives/config/ChadGPTConfig.java
@@ -10,7 +10,7 @@ public class ChadGPTConfig {
     public Properties readProperties(){
         Properties chadGPTProperties = new Properties();
         chadGPTProperties.put("endPoint", "https://api.openai.com/v1/completions");
-        chadGPTProperties.put("apiKey", "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");
+        chadGPTProperties.put("apiKey", "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");
         chadGPTProperties.put("promptPattern", "we have %s. If it is kind of food, or kind of book, or kind of medical product, answer \\\"yes\\\". otherwise, answer \\\"no\\\"");
 
         return chadGPTProperties;
